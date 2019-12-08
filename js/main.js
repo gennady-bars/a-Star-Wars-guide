7638d59b8c4ed02528fa7fa2f924a5ea60022d5d
gennady-bars <54933269+gennady-bars@users.noreply.github.com>
2019-12-08 15:52:56 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -25,6 +25,22 @@ let theme = {
         });
         localStorage.setItem('secondaryColor',this.secondaryColor);
         localStorage.setItem('themeType',this.themeType);
+    },
+    coolUpdate:function () {
+        let secondaryColor = localStorage.getItem('secondaryColor') || this.secondaryColor;
+        let themeType = localStorage.getItem('themeType') || this.themeType;
+
+        if (document.getElementById('edit')!==null){
+            document.getElementById('edit').style.backgroundColor = secondaryColor;
+        }
+        $('.content-wrapper')[0].classList.value = `content-wrapper ${themeType}`;
+        document.getElementById('nav').classList.value = `nav-wrapper navbar-fixed ${themeType}`;
+
+        $('.menu-btn')[0].childNodes.forEach(el => {
+            if (el.nodeType!==3){
+                el.style.backgroundColor = secondaryColor;
+            }
+        });
     }
   };
   
