4f66f8b3b6525c86c4d3035f2906e9c632682fdf
gennady-bars <54933269+gennady-bars@users.noreply.github.com>
2019-12-08 16:44:00 +0300

    disable theme.js

--- js/data-page.js
+++ js/data-page.js
@@ -1,5 +1,35 @@
-import {theme} from './theme.js';
+let theme = {
+   themeType:'indigo-theme',
+   secondaryColor:'#4ebdd4',
+   update:function () {
+       document.getElementById('edit').style.backgroundColor = this.secondaryColor;
+       $('.content-wrapper')[0].classList.value = `content-wrapper ${this.themeType}`;
+       document.getElementById('nav').classList.value = `nav-wrapper navbar-fixed ${this.themeType}`;
+       $('.menu-btn')[0].childNodes.forEach(el => {
+          if (el.nodeType!==3){
+              el.style.backgroundColor = this.secondaryColor;
+          }
+       });
+       localStorage.setItem('secondaryColor',this.secondaryColor);
+       localStorage.setItem('themeType',this.themeType);
+   },
+   coolUpdate:function () {
+       let secondaryColor = localStorage.getItem('secondaryColor') || this.secondaryColor;
+       let themeType = localStorage.getItem('themeType') || this.themeType;
 
+       if (document.getElementById('edit')!==null){
+           document.getElementById('edit').style.backgroundColor = secondaryColor;
+       }
+       $('.content-wrapper')[0].classList.value = `content-wrapper ${themeType}`;
+       document.getElementById('nav').classList.value = `nav-wrapper navbar-fixed ${themeType}`;
+
+       $('.menu-btn')[0].childNodes.forEach(el => {
+           if (el.nodeType!==3){
+               el.style.backgroundColor = secondaryColor;
+           }
+       });
+   }
+ };
 theme.coolUpdate();
 
 window.addEventListener('storage',function (event) {
