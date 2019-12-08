7d9c28ef9cefd09f9b0afb4ad561852923ff5fc3
gennady-bars <54933269+gennady-bars@users.noreply.github.com>
2019-12-08 18:33:59 +0300

    Update data-page.js

--- js/data-page.js
+++ js/data-page.js
@@ -2,7 +2,7 @@ $(function(){
    var includes = $('[data-include]');
    //document.querySelector("[data-include]").innerText;
    jQuery.each(includes, function(){
-      var file = 'components/' + $(this).data('include') + '.html';
+      var file = '../components/' + $(this).data('include') + '.html';
       $(this).load(file);
    });
 });
