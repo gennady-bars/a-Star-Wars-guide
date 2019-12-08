e5110b8d1aa3cb740d04706c7f68d96891582aeb
gennady-bars <54933269+gennady-bars@users.noreply.github.com>
2019-12-08 14:11:32 +0300

    new sword background

--- js/progresses.js
+++ js/progresses.js
@@ -1,8 +1,8 @@
 //шаблон для каждого скила
 function Skill(skill) {
     return `<div class="skill-container">
-                 <div style="flex: 1;">${skill.name}</div>
-                 <div class="progress-wrapper" style="margin-bottom: 0;flex: 4">
+                 <div style="flex: 2;">${skill.name}</div>
+                 <div class="progress-wrapper" style="margin-bottom: 0;flex: 6">
                       <div class="progress-line" style="background-color:${skill.color!==null?skill.color:'#4ebdd4'};width:${skill.val}">              
                       </div>
                  </div>
@@ -17,17 +17,17 @@ function SkillsBlock(skills){
 
 let skills = [
     {
-        name:'JavaScript',
+        name:'Сила   ',
         val:'80%',
         color:'#e74438'
     },
     {
-        name:'Vue.js',
+        name:'Честь   ',
         val:'85%',
         color:'rgba(16,255,182,0.78)'
     },
     {
-        name:'CSS',
+        name:'Отвага   ',
         val:'70%',
         color:'rgba(6,176,255,0.64)'
     }
